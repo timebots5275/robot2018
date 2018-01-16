dc2f2f3f9cd4f827be85c715b6543546edb7bab3
Steve Bowman <prongs95@gmail.com>
2018-01-16 16:00:58 -0600

    changed ports in RobotMap

--- src/org/usfirst/frc/team5275/robot/RobotMap.java
+++ src/org/usfirst/frc/team5275/robot/RobotMap.java
@@ -9,9 +9,9 @@ public class RobotMap {
 	// these integers represent the IDs of the CAN controllers
 	//TODO: change these ports to be correct
 	public static int frontLeftCAN = 1;
-	public static int frontRightCAN = 1;
-	public static int rearLeftCAN = 1;
-	public static int rearRightCAN = 1;
+	public static int frontRightCAN = 2;
+	public static int rearLeftCAN = 3;
+	public static int rearRightCAN = 4;
 	
 	
 }
