53f53a15b66cf7e0912b56fae0d1e18d556a7bdd
prongs95 <prongs95@gmail.com>
2018-02-16 20:04:51 -0600

    Changed How throttle works

--- src/org/usfirst/frc/team5275/robot/commands/TeleopDrive.java
+++ src/org/usfirst/frc/team5275/robot/commands/TeleopDrive.java
@@ -29,7 +29,8 @@ public class TeleopDrive extends Command {
 	protected void execute() {
 		//TODO: check that these axes are correct
 		val = (Robot.oi.driveStick.getRawAxis(1) * -1) * ((Robot.oi.driveStick.getRawAxis(3) + 1)/ 2);
-		Robot.drivetrain.drive.arcadeDrive(val, Robot.oi.driveStick.getRawAxis(2));
+		
+		Robot.drivetrain.drive.arcadeDrive(val, (Robot.oi.driveStick.getRawAxis(2)) * ((Robot.oi.driveStick.getRawAxis(3) + 1)/ 2));
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
